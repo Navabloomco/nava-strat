d4270820f82837fdab856c4ad040eec121490374
Navabloomco <contact@navabloomco.com>
2026-05-04 01:17:28 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+export const metadata = {
+  title: "Nava Strat",
+  description: "Fleet Intelligence Platform"
+};
+
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0 }}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
