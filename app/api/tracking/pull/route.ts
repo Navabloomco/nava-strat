3f964e4e57fc801708d193ac5cc1ee73767d39d4
Navabloomco <contact@navabloomco.com>
2026-05-06 10:59:50 +0300

    Update route.ts

--- app/api/tracking/pull/route.ts
+++ app/api/tracking/pull/route.ts
@@ -1,13 +1,23 @@
-import { supabaseAdmin } from "@/lib/supabaseAdmin";
+import { supabaseAdmin } from "../../../../lib/supabaseAdmin";
+
+export const dynamic = "force-dynamic";
 
 export async function GET() {
-  const { data, error } = await supabaseAdmin
-    .from("tracking_providers")
-    .select("*");
+  try {
+    const { data, error } = await supabaseAdmin
+      .from("tracking_providers")
+      .select("*");
 
-  return Response.json({
-    error,
-    data,
-    count: data?.length || 0,
-  });
+    return Response.json({
+      success: true,
+      error,
+      data,
+      count: data?.length || 0,
+    });
+  } catch (err: any) {
+    return Response.json({
+      success: false,
+      error: err.message,
+    });
+  }
 }
